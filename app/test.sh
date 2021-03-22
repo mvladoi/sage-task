551569d52d86bd9f9518bf796fa3205bc5b7f026
Vladoi Marian <mvladoi@ucsc.edu>
2021-03-22 10:59:48 +0100

    Update test.sh

--- app/test.sh
+++ app/test.sh
@@ -1,4 +1,5 @@
 
+
 #From APP ENGINE backend 
 
 # Get the homepage, testing purposed 
@@ -6,6 +7,9 @@ curl https://sage-task.nw.r.appspot.com
 
 # Get all the transactions 
 curl https://sage-task.nw.r.appspot.com/v1/api/transactions/
+
+# Get a pge of the transactions (50 rows)
+curl https://sage-task.nw.r.appspot.com/v1/api/transactions/page_num/5
 
 
 # Get transaction by symbol
@@ -67,11 +71,14 @@ curl https://sage-gateway-3hbojviu.nw.gateway.dev/?key=AIzaSyCdy-L8R_r-jv8gsB17a
 # Get all the transactions 
 curl https://sage-gateway-3hbojviu.nw.gateway.dev/v1/api/transactions/?key=AIzaSyCdy-L8R_r-jv8gsB17aWJxPN7pV7BggCo
 
-# Get transaction by symbol
-curl https://sage-gateway-3hbojviu.nw.gateway.dev/v1/api/transactions/get/symbol/VCRA?key=AIzaSyCdy-L8R_r-jv8gsB17aWJxPN7pV7BggCo
+# Get a pge of the transactions (50 rows)
+curl  https://sage-gateway-3hbojviu.nw.gateway.dev/v1/api/transactions/page_num/8?key=AIzaSyCdy-L8R_r-jv8gsB17aWJxPN7pV7BggCo
 
-# Get transaction by name 
+# Get transactions by name 
 curl https://sage-gateway-3hbojviu.nw.gateway.dev/v1/api/transactions/get/name/Vocera%20Communications,%20Inc.?key=AIzaSyCdy-L8R_r-jv8gsB17aWJxPN7pV7BggCo
+
+# Get transactions by symbol 
+curl https://sage-gateway-3hbojviu.nw.gateway.dev/v1/api/transactions/get/symbol/HHHH?key=AIzaSyCdy-L8R_r-jv8gsB17aWJxPN7pV7BggCo
 
 
 # Post transaction 
@@ -80,5 +87,21 @@ curl --header "Content-Type: application/json" \
      --data '{"amount":-0.845,"currency":"JKY","date":1604933435000,"name":"Just Another Test","symbol":"HHHH"}' \
      https://sage-gateway-3hbojviu.nw.gateway.dev/v1/api/transactions/add?key=AIzaSyCdy-L8R_r-jv8gsB17aWJxPN7pV7BggCo
 
+# Delete transactions by name 
+curl --header "Content-Type: application/json" \
+     --request DELETE \
+     --data '{}' \
+     https://sage-gateway-3hbojviu.nw.gateway.dev/v1/api/transactions/delete/name/Just%20Another%20Test?key=AIzaSyCdy-L8R_r-jv8gsB17aWJxPN7pV7BggCo 
 
 
+# Delete transactions by symbol
+curl --header "Content-Type: application/json" \
+     --request DELETE \
+     --data '{}' \
+     https://sage-gateway-3hbojviu.nw.gateway.dev/v1/api/transactions/delete/symbol/HHHH?key=AIzaSyCdy-L8R_r-jv8gsB17aWJxPN7pV7BggCo 
+
+# Update transaction 
+curl --header "Content-Type: application/json" \
+     --request PUT \
+     --data '{"amount":-0.000,"currency":"JKY","date":1604939999999,"name":"Vocera Task Updated","symbol":"VCRLL"}' \
+     https://sage-gateway-3hbojviu.nw.gateway.dev/v1/api/transactions/update/HHHH/JKY?key=AIzaSyCdy-L8R_r-jv8gsB17aWJxPN7pV7BggCo
